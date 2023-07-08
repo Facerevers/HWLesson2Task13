e18a6e6d8ce0c698717dcda6283ffb81f902b42a
crazle <crazle@mail.ru>
2023-07-08 15:40:17 +0300

    Вывод третьей цифры числа

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число и нажмите Enter");
+int number = int.Parse(Console.ReadLine());
+int result = -1;
+if (number < 100) {Console.WriteLine("Третьей цифры нет");}
+else
+{
+    while (number > 999)
+    {
+        number = number /10;
+    }
+    result = number % 10;
+Console.WriteLine($"{number} -> {result}");
+}
